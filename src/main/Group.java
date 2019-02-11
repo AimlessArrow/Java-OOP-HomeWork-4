1296b0853be0a7605799939314c8aa05ec982a81
Timur Beldenov <greycardinal@live.com>
2019-02-11 17:53:39 +0600

    First task (half complete)

--- src/main/Group.java
+++ src/main/Group.java
@@ -1,6 +1,9 @@
 package main;
 import java.util.Collections;
+import java.util.InputMismatchException;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 public class Group {
 	private Student[] studentGroup = new Student[10];
@@ -90,5 +93,63 @@ public class Group {
 		}
 		
 		return outputString;
+	}
+	
+	public void inputStudent() throws overflowException {
+		String firstName = "";
+		String lastName = "";
+		String patronymic = "";
+		String gender = "";
+		int age = 0;
+		int vacantIndex = -1;
+		Student newStudent = null;
+		String[] options = {"male", "female"};
+		
+		for(;;) {
+			try {
+				/* Getting first name */
+				firstName = JOptionPane.showInputDialog("Enter first name");
+				if(firstName == null) {
+					return;
+				}
+				/* Getting last name */
+				lastName = JOptionPane.showInputDialog("Enter last name");
+				if(lastName == null) {
+					return;
+				}
+				/* Getting patronymic */
+				patronymic = JOptionPane.showInputDialog("Enter patronymic");
+				if(patronymic == null) {
+					return;
+				}
+				/* Getting gender */
+				gender = JOptionPane.showOptionDialog(null, "Select gender", "Gender selection", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0])
+											== 0 ? options[0] : options[1];
+				/* Getting age */
+				age = Integer.parseInt(JOptionPane.showInputDialog("Enter age"));
+				break;
+			} catch(NullPointerException e) {
+				JOptionPane.showMessageDialog(null, "Input aborted");
+				return;
+			} catch(NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "Invalid input format");
+				continue;
+			} catch(InputMismatchException e) {
+				System.out.println("Invalid input format");
+			}
+		}
+		/* Saving results */
+		newStudent = new Student(firstName, lastName, patronymic, gender, age);
+		/* Inserting results into the group array */
+		for(int i = 0; i < this.studentGroup.length; i++) {
+			if(this.studentGroup[i] == null) {
+				vacantIndex = i;
+			}
+		}
+		if(vacantIndex == -1) {
+			throw new overflowException();
+		} else{
+			this.studentGroup[vacantIndex] = newStudent;
+		}
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,41 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		Group myGroup = new Group();
-		Student firstStudent = new Student("John", "Johnson", "John", "male", 23);
-		Student secondStudent = new Student("Fred", "Fredericson", "Frank", "male", 40);
-		Student thirdStudent = new Student("Helen", "Smith", "Veronica", "female", 35);
-		Student tempStudent = null;
-		int tempIndex = 0;
-		
+	
 		/* Populating student group */
 		try {
-			myGroup.addStudent(firstStudent);
-			myGroup.addStudent(secondStudent);
-			myGroup.addStudent(thirdStudent);
+			myGroup.addStudent(new Student("John", "Johnson", "John", "male", 23));
+			myGroup.addStudent(new Student("Fred", "Fredericson", "Frank", "male", 40));
+			myGroup.addStudent(new Student("Helen", "Smith", "Veronica", "female", 35));
 			myGroup.addStudent(new Student("Summer", "Times", "Victoria", "female", 33));
-		} catch(overflowException e) {
-			System.out.println("OverflowException caught");
-		}
-		System.out.println(myGroup);
-		/* Removing a student from the group*/
-		myGroup.removeStudent("Fredericson");
-		System.out.println(myGroup);
-		/* Searching for a student in the group */
-		tempIndex = myGroup.searchStudent("Times");
-		tempStudent = myGroup.getStudent(tempIndex);
-		System.out.println(tempStudent);
-		/* Overflow check */
-		try {
 			myGroup.addStudent(new Student("Ava", "Terry", "Evelyn", "female", 32));
 			myGroup.addStudent(new Student("Audrey", "McDonald", "Franklin", "male", 50));
 			myGroup.addStudent(new Student("Jasmine", "Bond", "Bond", "female", 23));
 			myGroup.addStudent(new Student("Rebecca", "Welch", "Victoria", "female", 47));
 			myGroup.addStudent(new Student("Ian", "Hardacre", "George", "male", 49));
-			myGroup.addStudent(new Student("Richard", "Harris", "James", "male", 35));
-			myGroup.addStudent(new Student("James", "Fraser", "Junior", "male", 58));
-			myGroup.addStudent(new Student("Andrea", "Peters", "Gloria", "female", 20));
 		} catch(overflowException e) {
-			/* e.printStackTrace(); */
+			System.out.println(e);
+		}
+		System.out.println(myGroup);
+		
+		try {
+			myGroup.inputStudent();
+		} catch(overflowException e) {
 			System.out.println(e);
 		}
 		System.out.println(myGroup);
