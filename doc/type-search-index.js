044c16f1bfd170732daee678b70a48690596f912
Timur Beldenov <greycardinal@live.com>
2019-03-11 11:51:30 +0600

    Garbage removed

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main","l":"Group"},{"p":"main","l":"Human"},{"p":"main","l":"Main"},{"p":"main","l":"overflowException"},{"p":"main","l":"Student"}]
